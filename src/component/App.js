2aac556bfa876bd9ef85433bd37694d3c982ba23
Cecil Johnson <cecil344@gmail.com>
2017-11-13 18:17:00 -0500

    changes to files

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import Grid from 'Grid';
+import List from './List';
 
 class App extends Component {
   render() {
